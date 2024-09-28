091ab7d7a10ca0a0f904e18f3b8355cf19f7de54
Yash Balotiya <131521010+balotiyash@users.noreply.github.com>
2024-09-28 21:55:42 +0530

    Add files via upload

---
+++ user/controller/OurWorkScript.js
@@ -0,0 +1,33 @@
+/** 
+ * File: user/controller/OurWorkScript.js
+ * Author: Yash Balotiya
+ * Description: // TODO
+ * Created on: 03/08/2024
+ * Last Modified: 03/08/2024
+*/
+
+// Window on ready
+$(document).ready(() => {
+    // Our Programs to handle proper onclick events on the sidebar
+    const allPrograms = [
+        "OurWork",
+        "Health", "HealthCare", "AYUSH", "Livelihoods",
+        "JobOrientedEducation", "Employment", "Technology", "HumanRights",
+        "SpeciallyAbled", "WomenEmpowerment", "ArtAndCulture", "AnimalProtection"
+    ];
+
+    for (let i = 0; i < allPrograms.length; i++) {
+        const mainElement = document.getElementsByClassName("ourProgramsChild2")[i];
+        const sideElement = document.getElementsByClassName("nav-link")[i];
+
+        if (mainElement && sideElement) { // Check if element exists to avoid errors
+            mainElement.addEventListener("click", () => {
+                window.location.href = `${allPrograms[i + 1]}.php`;
+            });
+
+            sideElement.addEventListener("click", () => {
+                window.location.href = `${allPrograms[i]}.php`;
+            });
+        }
+    }
+});
